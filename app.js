2fedaecd84fc5a72a38a0057ac088b61f4e7ab6c
Flor Farias <florencia.fariasmm@gmail.com>
2022-07-25 18:42:10 -0300

    Flor - instalando dependencias

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const method = require('method-override');
 const port = 3000;
 
 app.listen(port, () => console.log(`Servidor funcionando http://localhost:3000 ${port}!`));
@@ -14,7 +15,9 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, './src/views'));
 
-
+app.use(express.urlencoded({extended:false})); // req.body y el req.query
+app.use(express.json())
+app.use(method('m'));
 //Rutas
 app.use('/', mainRoutes);
 app.use('/usuarios',userRoutes);
