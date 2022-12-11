69654a2e665e3d4961b2e9fcf2e9eeeb3cc5a3ae
Flor Farias <florencia.fariasmm@gmail.com>
2022-12-11 20:41:07 -0300

    Flor - primer release de form user con express validator

--- src/validations/validacionUsuario.js
+++ src/validations/validacionUsuario.js
@@ -3,7 +3,93 @@ const { body } = require('express-validator');
 
 const register = [
 
-    body('nombre').notEmpty().withMessage('El nombre debe completarse').bail().isLength({min: 2}).withMessage('El nombre debe contener mínimo 2 caracteres').bail()
+    body('nombre')
+        .notEmpty()
+        .withMessage('El nombre debe completarse').bail().isLength({min: 2})
+        .withMessage('El nombre debe contener mínimo 2 caracteres')
+        .bail(),
+    body('apellido')
+        .notEmpty()
+        .withMessage('El apellido debe completarse')
+        .bail().isLength({min: 2})
+        .withMessage('El apellido debe contener mínimo 2 caracteres')
+        .bail(),
+    body('email')
+        .notEmpty()
+        .withMessage('El E-mail debe completarse')
+        .bail()
+        .isEmail()
+        .withMessage('Formato de E-mail inválido')
+        .bail(),
+    body('tel')
+        .optional()
+        .bail()
+        .isMobilePhone('es-AR').withMessage('Número telefónico incorrecto')
+        .bail(),
+    body('dni')
+        .notEmpty()
+        .withMessage('El DNI no puede quedar vacío')
+        .bail()
+        .isNumeric()
+        .withMessage('Solo se admiten números')
+        .bail(),
+    body('calle')
+        .notEmpty()
+        .withMessage('La calle no puede quedar vacía')
+        .bail(),
+    body('altura')
+        .notEmpty()
+        .withMessage('La altura no puede quedar vacía')
+        .bail()
+        .isNumeric()
+        .withMessage('Solo se admiten números')
+        .bail(),
+    body('localidad')
+        .notEmpty()
+        .withMessage('La localidad debe completarse')
+        .bail(),
+    body('provincia')
+        .notEmpty()
+        .withMessage('Seleccione una provincia')
+        .bail(),
+    body('cp')
+        .notEmpty()
+        .withMessage('Debe colocar su número de Código Postal')
+        .bail()
+        .isNumeric()
+        .withMessage('Solo se admiten números')
+        .bail(),
+    body('password')
+        .notEmpty()
+        .withMessage('Debe escribir una contraseña')
+        .bail()
+        .isLength({ min: 8 })
+        .withMessage("La contraseña debe contener mínimo 8 caracteres.")
+        .bail()
+        .isStrongPassword({
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1,
+            returnScore: false,
+        })
+        .withMessage(
+        'La contraseña debe contener al menos UNA letra mayuscula, UNA letra minuscula, UN número y UN caracter especial')
+        .bail(),
+
+  body("repetir_password")
+        .notEmpty()
+        .withMessage("Este campo es obligatorio.")
+        .bail()
+        .isLength({ min: 8 })
+        .withMessage("La contraseña debe contener mínimo 8 caracteres.")
+        .bail()
+        .custom(( value, { req } ) => {
+            if (value !== req.body.password) {
+                throw new Error('Las contraseñas deben coincidir');
+            }
+            return true;
+        }).bail(),
 ]
 
 module.exports = register;
